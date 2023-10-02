e90e23e8df891f071c0eb31f02c0f72c4640fadd
Evans <manotievance@gmail.com>
2023-10-02 08:26:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('#main').remove();
+
+const victoryHeader = document.querySelector('h1#victory');
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+newHeader.textContent = 'Manoti is the champion';
